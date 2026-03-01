38dbfd6b49718b47699967c0c2c473deb886d56e
Harpido-dev <apietsch94@gmail.com>
2026-03-02 00:06:34 +0100

    feature: upcoming event section
    - added upcoming event section
    - added onclick event on nav links to hide the menu

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-const hamMenu = document.querySelector(".ham-menu");
+const hamMenu = document.getElementById("ham-menu")
+const offScreenMenu = document.getElementById("off-screen-menu")
 
-const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 });
+
+function hideOfScreenMenu()
+{
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+}
